de8353caf3a48d778d703e0422850d1940fbf00c
eisynsayago <eisynsm@gmail.com>
2022-07-24 17:28:47 -0500

    agregando

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+let nombre = " "
+let apellido = " "
+let espacio = " "
+
+function Bienvenida(){
+
+    
+
+    nombre = prompt("Ingresa tu nombre por favor ") 
+    apellido = prompt (" Ingresa tu apellido por favor")
+
+alert( "Bienvenido : "+nombre + espacio + apellido)
+return nombre
+
+
+}
+
+Bienvenida()
+
+
+
+
+
+
+
